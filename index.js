5dd5eb28a7f63179cc20ecf69ccd4c68ed343856
Takeru Naito <takeru.naito@gmail.com>
2016-07-26 03:30:06 +0900

    Match strictly to the existing rules

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
 
 module.exports = {
   env: {
-    browser: true,
     es6: true,
-    node: true
+    browser: true
   },
   parserOptions: {
     ecmaVersion: 6,
@@ -12,19 +11,19 @@ module.exports = {
   },
   extends: "eslint:recommended",
   rules: {
-    "camelcase": "warn",
-    "eol-last": "warn",
+    "camelcase": ["warn"],
+    "eol-last": ["warn"],
     "eqeqeq": ["error", "smart"],
-    "indent": ["warn", 2],
+    "indent": ["error", 2],
     "linebreak-style": ["error", "unix"],
-    "no-console": "warn",
-    "no-debugger": "warn",
-    "no-spaced-func": "warn",
+    "no-console": ["warn"],
+    "no-debugger": ["warn"],
+    "no-spaced-func": ["warn"],
     "no-unused-vars": ["warn", { "args": "none" }],
-    "no-var": "error",
-    "quotes": ["warn", "double", {"avoidEscape": true, "allowTemplateLiterals": true}],
+    "no-var": ["error"],
+    "quotes": ["warn", "double", {"avoidEscape": true}],
     "semi": ["error", "never"],
     "space-before-function-paren": ["warn", "never"],
-    "strict": "error"
+    "strict": ["error"]
   }
 }
