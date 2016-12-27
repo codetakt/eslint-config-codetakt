c2fb25d369ac384e05f7ca6f4947b67fa9578ec3
Takeru Naito <takeru.naito@gmail.com>
2016-12-27 12:36:55 +0900

    Use the YAML form rulesets

--- index.js
+++ index.js
@@ -1,33 +1,7 @@
-/* eslint-env node */
+/* eslint-env es6,node */
 /* global module */
 
-"use strict"
+const readFileSync = require("fs").readFileSync
+const yaml = require("js-yaml")
 
-module.exports = {
-  env: {
-    es6: true,
-    browser: true
-  },
-  parserOptions: {
-    ecmaVersion: 6,
-    sourceType: "script"
-  },
-  extends: "eslint:recommended",
-  rules: {
-    "brace-style":["error", "1tbs", { "allowSingleLine": true }],
-    "camelcase": ["warn"],
-    "eol-last": ["warn"],
-    "eqeqeq": ["error", "smart"],
-    "indent": ["error", 2],
-    "linebreak-style": ["error", "unix"],
-    "no-console": ["warn"],
-    "no-debugger": ["warn"],
-    "no-spaced-func": ["warn"],
-    "no-unused-vars": ["warn", { "args": "none" }],
-    "no-var": ["error"],
-    "quotes": ["warn", "double", {"avoidEscape": true}],
-    "semi": ["error", "never"],
-    "space-before-function-paren": ["warn", "never"],
-    "strict": ["error"]
-  }
-}
+module.exports = yaml.safeLoad(readFileSync("./config.yml")) || {}
