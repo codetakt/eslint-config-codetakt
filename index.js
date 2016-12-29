8301a2830848e36e04635c7c1c23c4600ebf7901
Takeru Naito <takeru.naito@gmail.com>
2016-12-29 19:38:37 +0900

    Use the require-yaml package to resolve the path

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 /* eslint-env es6,node */
 /* global module */
 
-const readFileSync = require("fs").readFileSync
-const yaml = require("js-yaml")
-
-module.exports = yaml.safeLoad(readFileSync("./config.yml")) || {}
+require('require-yaml')
+module.exports = require('./config.yml')
